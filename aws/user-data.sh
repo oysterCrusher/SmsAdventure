ff174bc4fb3b8a18404a7fc1191b0eb8843de2f0
Immanuel Potter <manny.potter@matillion.com>
2018-10-27 17:04:42 +0100

    Quick and dirty bootstrap script

---
+++ aws/user-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Provision instance with necessary stuff on boot
+yum update
+yum install -y wget vim ruby
+cd /opt
+wget https://nodejs.org/dist/v8.10.0/node-v8.10.0-linux-x64.tar.xz
+tar -xf node-v8.10.0-linux-x64.tar.xz
+cd /home/ec2-user
+aws s3 cp s3://aws-codedeploy-us-west-1/latest/install .
+chmod +x ./install
+./install auto
