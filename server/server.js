6eaa0496a74187ce733426d3c00058a4a5508a99
j_adams <j.adams440@gmail.com>
2018-10-27 15:54:19 +0100

    More clockwork

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+'use strict';
+
+let apikey = process.env.CLOCKWORK_API_KEY
+let clockwork = require('clockwork')({key:apikey});
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -11,4 +16,14 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+
+//  clockwork.sendSms({ To: '<phone nuber>', Content: 'Test!'}, function(error, resp) {
+//    if (error) {
+//    	console.log('Something went wrong', error);
+//	} else {
+//		console.log('Message sent to',resp.responses[0].to);
+//		console.log('MessageID was',resp.responses[0].id);
+//	}
+});
+
 });
