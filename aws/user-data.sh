7dcc34ad3abbea81f315993c5588d51ee98da751
Immanuel Potter <manny.potter@matillion.com>
2018-10-27 17:30:00 +0100

    Moved nodejs binaries to /usr/local/bin

--- aws/user-data.sh
+++ aws/user-data.sh
@@ -7,6 +7,7 @@ yum install -y wget vim ruby unzip
 cd /opt
 wget https://nodejs.org/dist/v8.10.0/node-v8.10.0-linux-x64.tar.xz
 tar -xf node-v8.10.0-linux-x64.tar.xz
+mv node-v8.10.0-linux-x64.tar.xz/bin/* /usr/local/bin/
 cd /home/ec2-user
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
